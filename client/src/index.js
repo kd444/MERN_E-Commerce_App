17cc920e27e18e39e7719dad0229dab318ee7ad8
kd444 <karkeradarshan98@gmail.com>
2022-04-30 00:10:59 +0530

    Added Product Screen

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { HelmetProvider } from "react-helmet-async";
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
 // import data from '../data';
 const reducer = (state, action) => {
     switch (action.type) {
@@ -47,6 +48,9 @@ function HomeScreen() {
     }, []);
     return (
         <div>
+            <Helmet>
+                <title>Amazona</title>
+            </Helmet>
             <h1>Featured Products</h1>
             <div className="products">
                 {loading ? (
