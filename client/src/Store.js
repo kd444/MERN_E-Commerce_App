e74ab86b58195b5ab92003321fddcdb1da398e37
kd444 <karkeradarshan98@gmail.com>
2022-05-21 17:03:18 +0530

    Implemented place order action

--- client/src/Store.js
+++ client/src/Store.js
@@ -3,18 +3,17 @@ import { createContext, useReducer } from "react";
 export const Store = createContext();
 
 const initialState = {
-    userInfo: localStorage.getItem('userInfo')
-    ? JSON.parse(localStorage.getItem('userInfo'))
-    : null,
-
+    userInfo: localStorage.getItem("userInfo")
+        ? JSON.parse(localStorage.getItem("userInfo"))
+        : null,
 
     cart: {
-        shippingAddress: localStorage.getItem('shippingAddress')
-        ? JSON.parse(localStorage.getItem('shippingAddress'))
-        : {},
-        paymentMethod: localStorage.getItem('paymentMethod')
-        ? localStorage.getItem('paymentMethod')
-        : '',
+        shippingAddress: localStorage.getItem("shippingAddress")
+            ? JSON.parse(localStorage.getItem("shippingAddress"))
+            : {},
+        paymentMethod: localStorage.getItem("paymentMethod")
+            ? localStorage.getItem("paymentMethod")
+            : "",
         cartItems: localStorage.getItem("cartItems")
             ? JSON.parse(localStorage.getItem("cartItems"))
             : [],
@@ -42,31 +41,39 @@ function reducer(state, action) {
             localStorage.setItem("cartItems", JSON.stringify(cartItems));
             return { ...state, cart: { ...state.cart, cartItems } };
         }
-        case 'USER_SIGNIN':
+
+        case "CART_CLEAR":
+            return { ...state, cart: { ...state.cart, cartItems: [] } };
+
+        case "USER_SIGNIN":
             return { ...state, userInfo: action.payload };
-        case 'USER_SIGNOUT':
+
+        case "USER_SIGNOUT":
             return {
-              ...state,
-              userInfo: null,
-              cart: {
-                cartItems: [],
-                shippingAddress: {},
-                paymentMethod: '',
-              },
+                ...state,
+                userInfo: null,
+                cart: {
+                    cartItems: [],
+                    shippingAddress: {},
+                    paymentMethod: "",
+                },
             };
-        case 'SAVE_SHIPPING_ADDRESS':
+
+        case "SAVE_SHIPPING_ADDRESS":
             return {
-            ...state,
+                ...state,
                 cart: {
                     ...state.cart,
                     shippingAddress: action.payload,
                 },
             };
-        case 'SAVE_PAYMENT_METHOD':
+
+        case "SAVE_PAYMENT_METHOD":
             return {
-            ...state,
+                ...state,
                 cart: { ...state.cart, paymentMethod: action.payload },
-            };    
+            };
+
         default:
             return state;
     }
